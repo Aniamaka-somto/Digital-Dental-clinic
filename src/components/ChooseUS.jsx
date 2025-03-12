ff3166fd5482a927ace87b1f235972f4574c2c5b
Aniamaka_Somto <somtoaniams@gmail.com>
2025-03-12 01:02:25 +0100

    fix vercel img issue

--- src/components/ChooseUS.jsx
+++ src/components/ChooseUS.jsx
@@ -1,35 +1,45 @@
 import React from "react";
 import ChooseUsCard from "./ChooseUsCard";
+import img1 from "/src/assets/img1.jpg";
+import img2 from "/src/assets/img2.jpg";
+import img3 from "/src/assets/img3.jpg";
+import img4 from "/src/assets/img4.jpg";
 
 const ChooseUS = () => {
   const whyChooseUs = [
     {
+      Title: "Expert Care",
       content:
-        "Expert Care - Our experienced dentists use the latest techniques for comfortable, effective treatment.",
-      img: "src/assets/img1.jpg",
+        "Our experienced dentists use the latest techniques for comfortable, effective treatment.",
+      img: img1,
     },
     {
+      Title: "State-of-the-Art Technology",
       content:
-        "State-of-the-Art Technology - We invest in modern equipment for precise diagnostics and efficient treatments.",
-      img: "src/assets/img2.jpg",
+        "We invest in modern equipment for precise diagnostics and efficient treatments.",
+      img: img2,
     },
     {
+      Title: "Convenient Scheduling",
       content:
-        "Convenient Scheduling - We offer flexible appointment times including evenings and weekends to accommodate your busy lifestyle.",
-      img: "src/assets/img3.jpg",
+        "We offer flexible appointment times including evenings and weekends to accommodate your busy lifestyle.",
+      img: img3,
     },
     {
+      Title: "Personalized Treatment",
       content:
-        "Personalized Treatment - We create customized care plans to address your unique dental needs.",
-      img: "src/assets/img4.jpg",
+        "We create customized care plans to address your unique dental needs.",
+      img: img4,
     },
   ];
   return (
     <div className="h-fit px-10 pt-10 flex items-center flex-col ">
       <h1 className="pb-10 text-2xl font-bold">Why Choose US</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pb-10">
-        {whyChooseUs.map(({ content, img }, key) => {
-          return <ChooseUsCard content={content} img={img} key={key} />;
+        {whyChooseUs.map(({ content, img, Title }, key) => {
+          return (
+            <ChooseUsCard content={content} img={img} key={key} Title={Title} />
+          );
         })}
       </div>
     </div>
--- src/components/ChooseUsCard.jsx
+++ src/components/ChooseUsCard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-const ChooseUsCard = ({ img, content }) => {
+const ChooseUsCard = ({ Title, img, content }) => {
   return (
     <div className="ring-1 ring-emerald-200 lg:h-72 h-96  p-5 rounded-2xl shadow-md max-w-2xl grid grid-cols-1 lg:grid-cols-2 gap-2.5 items-center hover:scale-95 transition-transform">
-      <div className="w-full lg:h-full h-52 rounded-lg ring-1 ring-emerald-200 overflow-hidden shadow-sm ">
+      <div className="w-full lg:h-full h-52 rounded-lg ring-1 ring-emerald-200 overflow-hidden shadow-sm block">
         <img src={img} alt="" className=" aspect-auto w-full h-full" />
       </div>
-      {content}
+      <div className="">
+        <h1 className="font-semibold ">{Title}</h1>
+        {content}
+      </div>
     </div>
   );
 };
